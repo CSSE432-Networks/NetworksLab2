468761e8c73aa3b42ad3467607c6540ce5ace32d
Jesse Shellabarger <shellajt@rose-hulman.edu>
2017-03-30 23:47:45 -0400

    lab done

--- client/client.py
+++ client/client.py
@@ -1,14 +1,56 @@
 from socket import *
-
-
+import re
+import os
 
 serverName = raw_input("Enter the server's IP ")
 serverPort = int(raw_input("Enter the server's port "))
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-sentence = raw_input('Input lowercase sentence ')
-clientSocket.send(sentence)
-modifiedSentence = clientSocket.recv(1024)
-print 'From Server:', modifiedSentence
-clientSocket.close()
+while(1): 
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName, serverPort))
+    
+    command = raw_input(':> ')
+    if (command.startswith("iWant")):
+        clientSocket.send(command)
+        
+        confirm = clientSocket.recv(256)
+        if (not confirm == "Ready"):
+            print "Invalid iWant command"
+            break
+
+        file = open('received/'+command.split(" ")[1],'wb')
+
+        line = clientSocket.recv(1024)
+        while (line):
+            file.write(line)
+            line = clientSocket.recv(1024)
+        file.close()
+        print command.split(" ")[1] + " received"
+        
+    elif (command.startswith("uTake")):
+        clientSocket.send(command)
+        
+        confirm = clientSocket.recv(256)
+        if (not confirm == "Ready"):
+            print "Invalid uTake command"
+            break
+        
+        filename = command.split(" ")[1]
+        currentDir = os.getcwd()
+        qualifiedFilename = currentDir+'/received/'+filename
+        if (not os.path.isfile(qualifiedFilename)):
+            print "No such file exists."
+            break
+        
+        file = open(qualifiedFilename,'rb')
+        line = file.read(1024)
+        while (line):
+            clientSocket.send(line)
+            line = file.read(1024)
+            
+        file.close()
+        print filename + " sent to server"
+    else:
+        print "Invalid command"
+
+    clientSocket.close()
--- server/server.py
+++ server/server.py
@@ -1,14 +1,54 @@
-from socket import *
+# Networks lab2
+# Jesse Shellabarger & Tayler How
+
+import socket
+import re
 
 serverPort = int(raw_input("Enter the local port "))
-serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print 'The server is ready to receive'
 while 1:
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024)
+    command = connectionSocket.recv(256)
     
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence)
+    if(command.startswith('iWant')):
+        pattern = re.compile("iWant (.*)")
+        if(not pattern.match(command)):
+            print "Invalid iWant received"
+            connectionSocket.send("Not Ready")
+            break
+        connectionSocket.send("Ready")
+        filename = command.split(" ")[1]
+        file = open('store/'+filename,'rb')
+        line = file.read(1024)
+        while (line):
+            connectionSocket.send(line)
+            line = file.read(1024)
+        #serverSocket.shutdown(socket.SHUT_WR)
+        print filename + " sent to client"
+        file.close()
+        
+    elif(command.startswith('uTake')):
+        pattern = re.compile("uTake (.*)")
+        if(not pattern.match(command)):
+            print "Invalid uTake received"
+            serverSocket.send("Not ready")
+            break
+        
+        file = open('store/'+command.split(" ")[1],'wb')
+        
+        connectionSocket.send("Ready")
+        
+        line = connectionSocket.recv(1024)
+        while (line):
+            file.write(line)
+            line = connectionSocket.recv(1024)
+        file.close()
+        print command.split(" ")[1] + " received"
+        
+    else:
+        print "Invalid command received. Ignoring"
+
     connectionSocket.close()
